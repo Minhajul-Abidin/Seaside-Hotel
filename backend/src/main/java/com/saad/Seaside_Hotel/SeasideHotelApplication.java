f0d7e2bbeca76c1a07e74103b46bc3b2abf8bc8c
saad khan <saadkhan108b@gmail.com>
2024-06-08 17:47:49 +0530

    first commit

---
+++ backend/src/main/java/com/saad/Seaside_Hotel/SeasideHotelApplication.java
@@ -0,0 +1,13 @@
+package com.saad.Seaside_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasideHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasideHotelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/saad/Seaside_Hotel/SeasideHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.Seaside_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasideHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
