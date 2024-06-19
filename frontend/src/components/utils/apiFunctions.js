abbd8aa7c2fa63441fff3aa4adf81c89f6413129
saad khan <saadkhan108b@gmail.com>
2024-06-19 22:03:39 +0530

    Type fix in apiFunctions

--- frontend/src/components/utils/apiFunctions.js
+++ frontend/src/components/utils/apiFunctions.js
@@ -44,7 +44,7 @@ export async function getAllRooms(){
 // Function to delete room by id
 export async function deleteRoomById(id){
     try {
-        const response = api.delete("/rooms/${id}")
+        const response = api.delete(`/rooms/${id}`)
         return response.data
     } catch (error) {
         throw new Error("Error : Deleting room")
